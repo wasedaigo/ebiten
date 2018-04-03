4c3ba688e9401a041c93ee3654890e4190004af2
Hajime Hoshi <hajimehoshi@gmail.com>
2018-04-04 01:28:34 +0900

    graphics: Remove unused returning value from (*Image).Dispose

--- image.go
+++ image.go
@@ -550,7 +550,7 @@ func (i *Image) ColorModel() color.Model {
 // Note that important logic should not rely on At result since
 // At might include a very slight error on some machines.
 //
-// At can't be called before the main loop (ebiten.Run) starts (as of version 1.4.0-alpha).
+// At can't be called before the main loop (ebiten.Run) starts.
 func (i *Image) At(x, y int) color.Color {
 	if i.isDisposed() {
 		return color.RGBA{}
@@ -566,18 +566,15 @@ func (i *Image) At(x, y int) color.Color {
 // Dispose is useful to save memory.
 //
 // When the image is disposed, Dipose does nothing.
-//
-// Dipose always return nil as of 1.5.0-alpha.
-func (i *Image) Dispose() error {
+func (i *Image) Dispose() {
 	i.copyCheck()
 	if i.isDisposed() {
-		return nil
+		return
 	}
 	if !i.isSubimage() {
 		i.mipmap.dispose()
 	}
 	runtime.SetFinalizer(i, nil)
-	return nil
 }
 
 // ReplacePixels replaces the pixels of the image with p.
