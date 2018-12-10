c52676c66369ed3a439452cd54dd480b2012dfd2
Hajime Hoshi <hajimehoshi@gmail.com>
2018-12-11 00:32:21 +0900

    glfw/glfw: Remove unused files and add *_windows prefix to file names

---
+++ internal/glfw/glfw/build_windows.go
@@ -0,0 +1,12 @@
+package glfw
+
+/*
+// Windows Build Tags
+// ----------------
+// GLFW Options:
+#cgo windows CFLAGS: -D_GLFW_WIN32
+
+// Linker Options:
+#cgo windows LDFLAGS: -lopengl32 -lgdi32
+*/
+import "C"
--- internal/glfw/glfw/c_glfw_windows.go
+++ internal/glfw/glfw/c_glfw_windows.go
@@ -1,6 +1,12 @@
 package glfw
 
 /*
+#include "glfw/src/context.c"
+#include "glfw/src/init.c"
+#include "glfw/src/input.c"
+#include "glfw/src/monitor.c"
+#include "glfw/src/vulkan.c"
+#include "glfw/src/window.c"
 #include "glfw/src/win32_init.c"
 #include "glfw/src/win32_joystick.c"
 #include "glfw/src/win32_monitor.c"
